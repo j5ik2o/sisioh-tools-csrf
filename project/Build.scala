d1a9c729c7160e6d8e5caab71eab3eaea64dffba
じゅんいち☆かとう <j5ik2o@gmail.com>
2012-03-26 00:24:03 +0900

    first commit

---
+++ project/Build.scala
@@ -0,0 +1,24 @@
+import sbt._
+import sbt.Keys._
+
+object ProjectBuild extends Build {
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    "commons-codec" % "commons-codec" % "1.4",
+    "javax.servlet" % "servlet-api" % "2.4"
+  )
+
+  lazy val root = Project(
+    id = "root",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      name := "sisioh-tools-csrf",
+      organization := "org.sisioh",
+      version := "0.1-SNAPSHOT",
+      scalaVersion := "2.9.1",
+
+      libraryDependencies ++= appDependencies
+
+    )
+  )
+}
